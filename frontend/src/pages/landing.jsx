8ec12dd3b397844a3ce4e2ceaeab45c6673fbeb5
Akash Patel <ap8030462@bbdu.ac.in>
2024-08-11 09:59:57 +0530

    fronotend complete

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,52 +1,53 @@
 import React from 'react'
-import "../App.css";
+import "../App.css"
 import { Link, useNavigate } from 'react-router-dom'
+export default function LandingPage() {
 
 
-export default function landing() {
+    const router = useNavigate();
 
-  const router = useNavigate();
-  
-  return (
-    <div className='LandingPageContainer'>
-      <nav >
+    return (
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>Dev Video Call</h2>
+                </div>
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
 
-           <div className='navHeader'>
-              <h2>Dev Video Call</h2>
-           </div>
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
 
-           <div className='navlist'>
-            <p onClick={() => {
-              router("/aljk23")
-            }}>Join as Guest</p>
-            <p  onClick={() => {
-              router("/auth")
-            }} >Register</p>
-            <div onClick={() => {
-              router("/auth")
-            }} role='button'><p>Login</p></div>
-           </div>
-
-      </nav>
+                    }} role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
 
 
-        <div className="LandingMainContainer">
-          <div>
-            <h2><span style={{color:"orange"}}>Connent </span>with your loved Ones</h2>
+            <div className="landingMainContainer">
+                <div>
+                    <h2><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h2>
 
-            <p>Cover a distance by Boys Video Call</p>
-            <div role='button'>
-              <Link to={"/auth"}>Get Started</Link>
+                    <p>Cover a distance by Apna Video Call</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+
+                    <img src="/media/mobile.png" alt="" />
+
+                </div>
             </div>
-          </div>
 
-          <div>
-            <img src='/media/mobile.png' alt="moblie"/>
 
-          </div>
+
         </div>
-
-
-    </div>
-  )
+    )
 }
